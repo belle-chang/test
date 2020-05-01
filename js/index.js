f4fc71085121844e840d86b029e500d8df459357
Anabelle <bellech88@gmail.com>
2020-04-30 18:11:30 -0700

    customized cube and bg

--- js/index.js
+++ js/index.js
@@ -13,7 +13,8 @@ function resizeRendererToDisplaySize(renderer) {
 
 function main() {
   const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer({canvas});
+  // const renderer = new THREE.WebGLRenderer({canvas});
+  var renderer = new THREE.WebGLRenderer( { canvas, alpha: true } );
 
   const fov = 75;
   const aspect = 2;  // the canvas default
@@ -31,20 +32,22 @@ function main() {
     light.position.set(-1, 2, 4);
     scene.add(light);
   }
+  // scene.background = new THREE.Color(0xfcf3b8);
+  renderer.setClearColor( 0xfcf3b8, 0 ); 
 
   const boxWidth = 1;
   const boxHeight = 1;
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+  // const material = new THREE.MeshPhongMaterial({color: 0xd7b1f0});
+  const material = new THREE.MeshPhongMaterial({color: 0x5830c7});
+    // greenish blue
 
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
   function render(time) {
-    console.log(renderer)
-    console.log(time)
     time *= 0.001;  // convert time to seconds
 
     if (resizeRendererToDisplaySize(renderer)) {
@@ -66,7 +69,34 @@ function main() {
   }
   requestAnimationFrame(render);
 
+  // create an AudioListener and add it to the camera
+  var listener = new THREE.AudioListener();
+  camera.add( listener );
+
+  // create a global audio source
+  var sound = new THREE.PositionalAudio( listener );
+
+  // load a sound and set it as the Audio object's buffer
+  var audioLoader = new THREE.AudioLoader();
+  audioLoader.load( 'sounds/walk_away.mp3', function( buffer ) {
+    sound.setBuffer( buffer );
+    sound.setLoop(true);
+    sound.setVolume(0.25);
+    sound.play();
+  });
+
+  cube.add(sound);
+
+  // create an AudioAnalyser, passing in the sound and desired fftSize
+  var analyser = new THREE.AudioAnalyser( sound, 32 );
+
+  // get the average frequency of the sound
+  var data = analyser.getAverageFrequency();
+  console.log(data);
+
 }
+
+
 
 
 
